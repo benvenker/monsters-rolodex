294cd4b890fbd095e0c5a4611dbc8e61c723f6d3
Ben Venker <bvemails@gmail.com>
2019-09-23 06:18:44 -0700

    Updates

---
+++ precache-manifest.1f0f68296214ac05d6f4f409df7dfb1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013d0de4a8859b4dd8c5a1d7a9293b39",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9acfcd75a005c5a86414",
+    "url": "/monsters-rolodex/static/css/main.373f6935.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "9acfcd75a005c5a86414",
+    "url": "/monsters-rolodex/static/js/main.dd6880c7.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
